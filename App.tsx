39ffd952c6c6592bce51231d08fee5f235c56774
Kevinreisdacruz <kevinreisdacruz10@gmail.com>
2025-05-08 17:29:47 -0300

    foi feito comentarios pelo codigo, e esta sendo uma forma de mostrar informacoes do pokemon selecionado

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import styles from './assets/css/styles';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import Pokemon from './src/components/InfosPoke';
 
-const App: React.FC = (props) => {
+const App: React.FC = () => {
 
   const [pokemons, setPokemons] = useState([])
-  const [infosPoke, setInfosPoke] = useState('')
+  const [infosPoke, setInfosPoke] = useState('');
+
   const [campoPesquisa, setCampoPesquisa] = useState("")
-  
+
 
   const [modal, setModal] = useState(false)
 
@@ -17,16 +18,25 @@ const App: React.FC = (props) => {
     fetchPokemons()
   }, [])
 
-  useEffect(() => {
-    console.log(infosPoke);
-  },[infosPoke])
+  // useEffect(() => {
+  //   console.log(infosPoke);
+  // },[infosPoke])
 
 
   const fetchPokemons = () => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=1000')
-      .then(response => response.json())
-      .then(pokemons => setPokemons(pokemons.results))
+    .then(response => response.json())
+    .then(pokemons => setPokemons(pokemons.results))
+
+    // foi feita uma requisicao http para a pokeApi e salvando a lista de pokemons na variavel de estado pokemons
+
+    // fetch: ele faz uma requisicao para a url da pokeapi pedindo um limit de informacoes de 1000 pokemons  
+
+    // .then(response => response.json()): converte a resposta da api em algo mais legivel via codigo
+
+    // .then(pokemons => setPokemons(pokemons.results)): quando o json estiver legivel o pokemons.result acessa um array com todos os 1000 pokemons que foram requisitados no fetch, depois a funcao setPokemons muda o estado da minha variavel pokemons e salva os pokemons no seu estado.
   }
+
 
 
   return (
@@ -34,13 +44,26 @@ const App: React.FC = (props) => {
     <ScrollView style={styles.root}>
 
       <Pokemon visibleModal={modal} mudaVisibilidade={() => setModal(false)} PokemonSelecionado={infosPoke} />
-      
+
+      {/* visibleModal={modal}
+      O que é: Está passando a variável modal como prop chamada visibleModal.
+      oq ele faz: Controla se o modal deve estar visível ou não. Lá no InfosPoke */
+
+        // mudaVisibilidade = {() => setModal(false)}
+        // O que é: Está passando uma função como prop chamada mudaVisibilidade.
+
+        // Fecha o modal, pois define modal como false.
+
+        // para que serve: Quando o usuário acionar a funcao setModal, o InfosPoke pode chamar a função infosPoke para guardar o pokemon e enviar ao component InfosPoke.
+
+      }
+
       <View style={styles.container}>
 
         {pokemons.map((pokemons, numeracao) => {
           return (
 
-            <TouchableOpacity style={styles.btn} onPress={() => {setModal(true), setInfosPoke(String(pokemons.name)) }}>
+            <TouchableOpacity style={styles.btn} onPress={() => { setModal(true), setInfosPoke(String(pokemons.name)) }}>
               <Image
                 style={{ width: 150, height: 150 }}
                 source={{
@@ -52,6 +75,15 @@ const App: React.FC = (props) => {
 
           )
         }
+
+          //{pokemons.map((pokemons,numeracao) => { : o map esta percorrendo o array da variavel de estado pokemons que foi requisitado na PokeApi
+
+          //<TouchableOpacity style={styles.btn} onPress={() => { setModal(true), setInfosPoke(String(pokemons.name)) }}> : foi aplicado um onpress que ao clicar sera acionado a minha funcao setModal e definira o estado da minha variavel de estado de false para true onde ela ira abrir minha modal.
+
+          // setInfosPoke(String(pokemons.name)) :  salva o nome do pokemon selecionado para ser enviado para minha modal 
+
+          // <Text style={styles.nomes}>{numeracao + 1} - {pokemons.name}</Text> : esta mostrando na tela a numeracao dos pokemons dianta da sua posicao no array, o +1 esta sendo usado para a numeracao nao comecar na contagem 0, e tambem esta sendo mostrado os nomes dos pokemons que estao no array 
+
 
         )}
 
--- src/components/InfosPoke.tsx
+++ src/components/InfosPoke.tsx
@@ -10,39 +10,94 @@ interface infosPokemon {
     PokemonSelecionado: String,
 }
 
-const Pokemon: React.FC = (props: infosPokemon) => {
 
-    const [caractPoke, setcaractPoke] = useState([])
+
+
+
+
+//interface : ele é tipo um molde que define como um objeto deve ser estruturado.
+
+//visibleModal: boolean, define se e verdadeiro ou falso, ou seja, se a modal esta visivel ou nao
+
+// mudaVisibilidade: () => void : isso e uma propriedade sem parametro(void = NADA), esta sendo usada para fechar o modal.
+
+//pokemonSelecionado: contem o nome do pokemon selecionado, adquiradida pelo meu estado pokemons no meu App.tsx e foi passada em um parametro de string
+
+// tipar: definir um tipo para algo como por exemplo  PokemonSelecionado: String, eu tipei o PokemonSelecionado pra ele receber um parametro de String.
+
+
+
+const Pokemon: React.FC<infosPokemon> = (props) => {
+
+    // React.FC <infosPokemon> = (props) => { :
+    // meu componente funcional esta recebendo um pros com a estrutura da interface.
+
+    const [pokemon, setPokemon] = useState([]);
+    
+const fetchDescription = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.PokemonSelecionado}/`)
+    .then((response) => response.json())
+    .then((response) => setPokemon(response));
+}
+
+
+
+
+    // const [caractPoke, setcaractPoke] = useState([])
+    // //esta guradando as caracteristicas do pokemon da PokeApi
+
     const [imagemPoke, setImagePoke] = useState()
 
-    useEffect(() => {
-        fetchPokemon()
-    },[])
+    // useEffect(() => {
+    //     fetchPokemon()
+    // }, [])
 
-        const fetchPokemon = () => {
-           fetch (`https://pokeapi.co/api/v2/characteristic/${props.PokemonSelecionado}/`)
-           .then(response => response.json())
-           .then(caractPoke => setcaractPoke(caractPoke.results))
-        }
+    //este useEffect: chama o fetchPokemon, para busacr as caracteristicas do pokemon
 
-    return(
+    // const fetchPokemon = () => {
+    //     fetch(`https://pokeapi.co/api/v2/characteristic/${props.PokemonSelecionado}/`)
+    //         .then(response => response.json())
+    //         .then(caractPoke => setcaractPoke(caractPoke.results))
 
-    <Modal visible={props.visibleModal}>
-        <TouchableOpacity onPress={props.mudaVisibilidade}>
-            <Text>X</Text>
-        </TouchableOpacity>
+    //     // fetch: ele faz uma requisicao para a url da PokeApi pedindo as caracteristicas do pokemon selecionado. 
+    //     // O nome do pokemons esta vindo do PokemonSelecionado ques etsa sendo passado do meu app.tsx. 
+    //     // O resultado e salvo no minha variavel de estado caracPoke dentro do array. 
 
-        <Image
+
+    // }
+
+    return (
+
+        <Modal visible={props.visibleModal}>
+            <TouchableOpacity onPress={props.mudaVisibilidade}>
+                <Text>X</Text>
+            </TouchableOpacity>
+
+            {/* este TouchableOpacity esta fechandao meu modal, passando a minha propriedade mudaVisibilidade que tem seu valor dada como falsa, oq faz ele fechar a modal quando acionada  */}
+
+
+            <Image
                 style={{ width: 150, height: 150 }}
                 source={{
-                  uri: `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${props.PokemonSelecionado}.png`
+                    uri: `https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/${props.PokemonSelecionado}.png`
                 }}
-              />
-        <Text>{props.PokemonSelecionado}</Text>
-    </Modal>
+
+            //a imagem esta usando a propriedade PokemonSelecionado para mostrar a imagem diante do nome do pokemon.
+            />
+
+                <TouchableOpacity onPress={() => fetchDescription()}>
+                    <Text>TESTAR</Text>
+                </TouchableOpacity>
+            <Text>{props.PokemonSelecionado}</Text>
+            {pokemon.map((details, index) => (
+                <Text>{index}</Text>
+            ))}
+
+            {/* mostra o nome do pokemon selecionado no app.tsx dentro da modal */}
+        </Modal>
 
     )
-    
+
 }
 
 export default Pokemon;
